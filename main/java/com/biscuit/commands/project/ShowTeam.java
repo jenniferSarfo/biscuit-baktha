cb225b90c0cee019f7c50f747dfb1ac270dd0226
Pavithra Ravilla (Student) <pravill1@sundevils.asu.edu>
2022-02-24 01:30:38 -0700

    US-51-Task-61:Retrieving the team members in the project when needed

---
+++ main/java/com/biscuit/commands/project/ShowTeam.java
@@ -0,0 +1,40 @@
+package com.biscuit.commands.project;
+
+import java.io.IOException;
+
+import com.biscuit.ColorCodes;
+import com.biscuit.commands.Command;
+import com.biscuit.models.Project;
+
+public class ShowTeam implements Command 
+{
+
+	Project p = null;
+
+
+	public ShowTeam(Project p)
+	{
+		super();
+		this.p = p;
+	}
+
+
+	@Override
+	public boolean execute() throws IOException
+	{
+
+		System.out.println(ColorCodes.BLUE + "Team Members: " + ColorCodes.RESET);
+
+		String[] teamMembers = p.nameOfTeammembers;
+
+		for(int i=0; i<teamMembers.length;i++)
+		{
+			System.out.println(ColorCodes.RESET + teamMembers[i]);
+		}
+		System.out.println();
+
+		return true;
+	}
+
+
+}
