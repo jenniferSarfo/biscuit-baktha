8cdd9d677d15897efbe73b5199d18b8e75b26b31
sahithiboddupalli <90217615+sahithiboddupalli@users.noreply.github.com>
2022-03-04 23:06:05 -0700

    Update ViewTest.java

--- test/java/com/biscuit/views/ViewTest.java
+++ test/java/com/biscuit/views/ViewTest.java
@@ -3,18 +3,17 @@ package com.biscuit.views;
 import java.awt.Button;
 import java.io.File;
 
-import org.junit.Test;
 
 import junit.framework.TestCase;
 
 public class ViewTest extends TestCase {
-	@Test
+	
 	public void test() {
 		Button btn = new Button();
 		assertNotNull(btn);
 			
 	}
-	@Test
+	
 	public void test1() {
 		String userHome = System.getProperty("user.home");
 
